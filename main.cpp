faad90f664366ad6dd71efc87f86d3ff9c348598
Muralidhar Nalabothula <44350281+ronald00773@users.noreply.github.com>
2021-10-03 11:41:28 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// This code is written by Muralidhar Nalabothula 
+// Please conside citing https://journals.aps.org/prb/abstract/10.1103/PhysRevB.102.045416 if you use the code.
+// contact me in case of bug reports : muralidharrsvm7@gmail.com 
+#include "purcell.h"
+
+int main(){
+    Print_details();
+    double dipole_distance = 1.6*1e-9;
+    System elements = { Interface(1.0,  1.85, 0.0, 0.0, 0.0, 0.0), Dielectric_slab(1.85, 5*1e-9) ,Interface(1.85,  1.5, 4.915721315948482e-07+3.596760413126592e-05*I, 0.0, 0.0,  1.0021936438284495e-07+8.464171939906124e-06*I) };
+    //                  Air-dielectric interface                     dielectric slab               dielectric, 2D material and substate interface
+    auto ppp = Purcell(1145476.66561009,elements, dipole_distance ,"xx");//}
+    std::cout<< "Total decay rate (Normalized to free decay rate) =                    " <<ppp.total  <<std::endl;
+    std::cout<< "Total radiative decay rate (Normalized to free decay rate) =          " <<ppp.rad  <<std::endl;
+    std::cout<< "Total non-radiative rate (Normalized to free decay rate) =            " <<ppp.non_rad  <<std::endl;
+    std::cout<< "Relative numerical integration error for radiative calulation =       " <<ppp.err_r  <<std::endl;
+    std::cout<< "Relative numerical integration error for non-radiative calulation =   " <<ppp.err_nr  <<std::endl;
+    return 0;
+}
---
+++ purcell.h
@@ -0,0 +1,69 @@
+// This code is written by Muralidhar Nalabothula 
+// Please conside citing https://journals.aps.org/prb/abstract/10.1103/PhysRevB.102.045416 if you use the code.
+// contact me in case of bug reports : muralidharrsvm7@gmail.com 
+#pragma once
+//Std headers
+#include <iostream>
+#include <complex>
+#include <cmath>
+#include <chrono>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+void Print_details();
+// Defining Constants 
+const std::complex<double> I(0.0f,1.0f);
+const std::complex<double> c_zero(0.0f,0.0f);
+const std::complex<double> c_one(1.0f,0.0f);
+const double pi = 3.14159265358979323846 ;
+const int precision =4;
+const double eta_0 = 376.991118430775;
+
+struct struct_elements { std::string element_type; /* Structure to store input data of the complete system*/
+                         std::complex<double> nl; std::complex<double> nr; 
+                         std::complex<double> sxx_p; std::complex<double> sxy_p; 
+                         std::complex<double> syx_p; std::complex<double> syy_p;
+                         std::complex<double> nd; double thickness; 
+                         virtual ~struct_elements(){}};
+
+struct Interface : struct_elements { /* Structure to store sinlge inteface input data of the system*/
+                                     Interface(std::complex<double> nl_, std::complex<double> nr_, 
+                                     std::complex<double> sxx_p_, std::complex<double> sxy_p_, 
+                                     std::complex<double> syx_p_, std::complex<double> syy_p_)
+                                     {
+                                        element_type = "interface";
+                                        nl = nl_;
+                                        nr = nr_;
+                                        sxx_p = sxx_p_ * eta_0;
+                                        sxy_p = sxy_p_ * eta_0;
+                                        syx_p = syx_p_ * eta_0;
+                                        syy_p = syy_p_ * eta_0;
+                                     }
+
+                                      ~Interface(){}
+                                     
+                                     };
+
+struct Dielectric_slab : struct_elements { /* Structure to store dielectric input data of the system*/
+                                           Dielectric_slab(std::complex<double> nd_, double thickness_)
+                                           {
+                                               element_type = "Dielectric";
+                                               nd = nd_;
+                                               thickness = thickness_;
+                                           } 
+                                           ~Dielectric_slab(){}
+                                           };
+
+typedef std::vector<struct_elements> System;
+
+/* Output Structure to store computed decay rates (and their relative errors) of the system
+It contains transface matrix and propagation angle*/
+struct Decay_rates {double total; double rad; double non_rad; double err_r; double err_nr ;};
+
+Decay_rates Purcell(double k0, /* Computing the rad and non rad decay rates by perfoming the kp integral*/
+                    std::vector<struct_elements> elements,
+                    double z0 , 
+                    std::string dir, 
+                    size_t limit = 128,size_t m_deg = 10);
+
